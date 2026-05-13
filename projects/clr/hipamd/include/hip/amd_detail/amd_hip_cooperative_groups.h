098f66813d5031f17b6dbe382d12772f37706b97
Gerardo Hernandez <gerardo.hernandez@amd.com>
2026-05-13 14:19:25 +0100

    AIRUNTIME-171 - fix up previous commits

--- projects/clr/hipamd/include/hip/amd_detail/amd_hip_cooperative_groups.h
+++ projects/clr/hipamd/include/hip/amd_detail/amd_hip_cooperative_groups.h
@@ -1400,7 +1400,7 @@ struct isBooleanFunc<T, cooperative_groups::bit_xor<T>> : __hip_internal::true_t
 template <typename TyGroup>
 __CG_QUALIFIER__ unsigned long long groupMask(const TyGroup& group)
 {
-  unsigned long long mask;
+  unsigned long long mask = ~0ull;
 
   if constexpr (impl::isCoalescedGroup<TyGroup>::value) {
     // for coalesced_groups, the mask is simply the activemask
