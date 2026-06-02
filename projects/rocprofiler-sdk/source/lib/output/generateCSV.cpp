0f20ff89018dc218f646e12e7ce5e4dc18ff2486
EvanPhoukong <Evan.Phoukong@amd.com>
2026-06-02 20:35:30 +0000

    rocprofv3: Support rocSHMEM EXT buffer records (args + retval)

--- projects/rocprofiler-sdk/source/lib/output/generateCSV.cpp
+++ projects/rocprofiler-sdk/source/lib/output/generateCSV.cpp
@@ -762,10 +762,10 @@ generate_csv(const output_config&                                           cfg,
 }
 
 void
-generate_csv(const output_config&                                               cfg,
-             const metadata&                                                    tool_metadata,
-             const generator<rocprofiler_buffer_tracing_rocshmem_api_record_t>& data,
-             const stats_entry_t&                                               stats)
+generate_csv(const output_config&                                                   cfg,
+             const metadata&                                                        tool_metadata,
+             const generator<rocprofiler_buffer_tracing_rocshmem_api_ext_record_t>& data,
+             const stats_entry_t&                                                   stats)
 {
     if(data.empty()) return;
 
