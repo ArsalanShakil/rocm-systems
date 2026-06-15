4be9c8854f816a41cf364a353e2707ff45be7158
josnarlo <joseph.narlo@amd.com>
2026-06-15 16:37:32 -0500

    Remove hard coded error code
    Signed-off-by: josnarlo <joseph.narlo@amd.com>

--- projects/amdsmi/amdsmi_cli/amdsmi_parser.py
+++ projects/amdsmi/amdsmi_cli/amdsmi_parser.py
@@ -777,8 +777,7 @@ class AMDSMIParser(argparse.ArgumentParser):
             def __call__(self, parser, args, values, option_string=None):
                 if args.watch is None:
                     raise argparse.ArgumentError(
-                        self,
-                        f"invalid argument: '{self.dest}' needs to be paired with -w/--watch. Error code: 202",
+                        self, f"invalid argument: '{self.dest}' needs to be paired with -w/--watch."
                     )
                 else:
                     setattr(args, self.dest, values)
